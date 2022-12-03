237a39e247a69a3f5124aa8a70f6f39c7e9bf34f
Vladimir Sokurov <sokurov.va@yandex.ru>
2022-12-03 11:40:23 +0300

    +

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +7,16 @@ def index(request):
 
 
 def show_catalog(request):
-    template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sorting = {'name': 'name',
+               'min_price': 'price',
+               'max_price': '-price'}
+    sort_method = request.GET.get('sort', 'name')
+    phones = Phone.objects.order_by(sorting.get(sort_method))
+    context = {'phones': phones}
+    return render(request, 'catalog.html', context)
 
 
 def show_product(request, slug):
-    template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
+    return render(request, 'product.html', context)
