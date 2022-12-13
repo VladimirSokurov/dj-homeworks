5171be0619b799db46f44ea59b9fab1d64377f92
Vladimir Sokurov <sokurov.va@yandex.ru>
2022-12-13 20:40:02 +0300

    +

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, ArticleTag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    is_main_check = 0
+
+    def clean(self):
+        for form in self.forms:
+            if form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    self.is_main_check += 1
+        if self.is_main_check != 1:
+            raise ValidationError('Основной раздел должен быть только один')
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = ArticleTag
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+
+
+admin.site.register(Tag)
